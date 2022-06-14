fee15e54ea2207091c53cdca6416289e6111f884
Tekop1223 <ertec@live.dk>
2022-06-14 11:17:19 +0200

    first commit

---
+++ DarkSoulsLikeGame/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Tekop
+{
+    public class Item : ScriptableObject
+    {
+        [Header("Item Infomation")]
+        public Sprite itemIcon;
+
+        public string itemName;
+
+    }
+
+    
+}
